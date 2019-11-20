01a730623089fdce7802645e4b5179125e2cf9fd
Arthur Baars <arthur@semmle.com>
2019-11-20 16:15:27 +0100

    silly alert

--- cmd/prototool/main.go
+++ cmd/prototool/main.go
@@ -29,3 +29,12 @@ import (
 func main() {
 	os.Exit(cmd.Do(os.Args[1:], os.Stdin, os.Stdout, os.Stderr))
 }
+func controller(msg string) {
+        if msg == "start" {
+                panic("Message not understood.")
+        } else if msg == "start" {
+                panic("Message not understood.")
+        } else {
+                panic("Message not understood.")
+        }
+}
